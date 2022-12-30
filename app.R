db5d91ec288441829c5497ebff331444932863a9
armelsanou <armelsauvy00@gmail.com>
2022-12-30 13:48:06 +0100

    Ajout bloc serveur et ui

--- app.R
+++ app.R
@@ -4,3 +4,13 @@ library(DT)
 library(dplyr)
 library(tidytext)
 library(wordcloud)
+
+
+#Ajout partie serveur
+server <- function(input, output, session) {
+
+}
+
+
+#Partie ui
+shinyApp(ui = ui, server = server)
